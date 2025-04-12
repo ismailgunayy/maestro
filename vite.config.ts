e06701c3295d00e4749b3001d2405f316df3a7e9
İsmail Günay <ismailggunay@gmail.com>
2025-04-12 21:11:46 +0200

    specify port

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: 'modern-compiler',
+export default ({ mode }: { mode: string }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	// import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+	// import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+	return defineConfig({
+		plugins: [react()],
+		css: {
+			preprocessorOptions: {
+				scss: {
+					api: 'modern-compiler',
+				},
 			},
 		},
-	},
-});
+		server: {
+			port: Number(process.env.VITE_PORT) || 3023,
+		},
+	});
+};
